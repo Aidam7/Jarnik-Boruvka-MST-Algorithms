3bd3e1af652843437cd366059d2287c99410e106
Tomáš Diblík <dibla.tomas@post.cz>
2023-03-18 15:46:21 +0100

    [Chore] Change graph used, just to make sure

--- implementation/boruvkuv-alg/Program.cs
+++ implementation/boruvkuv-alg/Program.cs
@@ -1,23 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace boruvkuv_alg
+﻿namespace boruvkuv_alg
 {
     internal class Program
     {
         static void Main()
         {
-            MSTGraph graph = new MSTGraph(6);
+            /*
+                    0
+                   / \
+                  1 - 2
+                 / \ / \
+                3   4 - 5
+                 \ /
+                  6
+            */
+            MSTGraph graph = new MSTGraph(7);
 
             graph.AddEdge(0, 1, 4);
             graph.AddEdge(0, 2, 3);
-            graph.AddEdge(1, 2, 1);
-            graph.AddEdge(1, 3, 2);
-            graph.AddEdge(2, 3, 4);
-            graph.AddEdge(3, 4, 2);
-            graph.AddEdge(4, 5, 6);
 
-            var MinimumSpanningTree = graph.BoruvkaMST();
+            graph.AddEdge(1, 2, 5);
+            graph.AddEdge(1, 3, 2);
+
+            graph.AddEdge(2, 4, 8);
+            graph.AddEdge(2, 5, 1);
+
+            graph.AddEdge(3, 6, 8);
+
+            graph.AddEdge(4, 5, 6);
+            graph.AddEdge(4, 6, 2);
+
+            List<Edge> MinimumSpanningTree = graph.BoruvkaMST();
 
             Console.WriteLine("Minimum spanning tree:");
             foreach (Edge edge in MinimumSpanningTree)
