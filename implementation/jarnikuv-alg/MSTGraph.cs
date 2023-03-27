f3333f2b5d8a2192c5d3059bd40a33b259595957
Vilem <vilem.cerman@seznam.cz>
2023-03-27 13:03:32 +0200

    add Edge and  MSTGraph classes

---
+++ implementation/jarnikuv-alg/MSTGraph.cs
@@ -0,0 +1,57 @@
+﻿namespace jarnikuv_alg
+{
+    public class MSTGraph
+    {
+        private readonly int number_of_nodes;
+        private readonly List<Edge> edges = new List<Edge>();
+
+        public MSTGraph(int _number_of_nodes)
+        {
+            number_of_nodes = _number_of_nodes;
+        }
+
+        public void AddEdge(int from, int to, int weight) => edges.Add(new Edge(from, to, weight));
+
+        public List<Edge> JarnikMST()
+        {
+            /*
+             * node[] jarnikAlgorithm(graph, weight)
+                Queue q //fronta
+                q.addAllVetices(graph.vertices) //pridej vsechny uzly do fronty
+    
+                distances = new int[q.size()] //pole vzdalenosti
+                distances[0] = 0 //koren
+                for i in 1 -> distances.length - 1 do                                 
+                    distances[i] = +inf //ostatni uzly jsou nekonecne daleko
+     
+                predecessors = new node[q.size()] //pole predchudcu
+                predecessors[0] = null //koren nema predchudce
+    
+                while !queue.empty() do //dokud neni fronta prazdna
+                    u = queue.extractMin() //vrat prvek v minimalni vzdalenosti
+                    for node in descendants(u) do //pro vsechny potomky u
+                        if queue.contains(node) AND weight(u, node) < d[node] //pokud se nektery z potomku priblizil k dosud postavene kostre
+                            then predecessors[node] = u //u je tedy jeho predek
+                                 d[node] = weight(u, node) //nastav nvou vzdalenost    
+    
+                return predecessors //vrat pole predchudcu
+             */
+
+
+
+            List<Edge> MST = new List<Edge>();
+
+            // Create a parent array to keep track of components
+            int[] parent = new int[number_of_nodes];
+            for (int i = 0; i < number_of_nodes; i++)
+            {
+                parent[i] = i;
+            }
+
+            return MST;
+        }
+
+        // Find the parent of a node in the parent array
+        private int find_parent_node(int[] parent, int i) => parent[i] == i ? i : (parent[i] = find_parent_node(parent, parent[i]));
+    }
+}
--- implementation/jarnikuv-alg/Program.cs
+++ implementation/jarnikuv-alg/Program.cs
@@ -3,8 +3,40 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {/*
+                    0
+                   / \
+                  1 - 2
+                 / \ / \
+                3   4 - 5
+                 \ /
+                  6
+            */
+            MSTGraph graph = new MSTGraph(7);
+
+            graph.AddEdge(0, 1, 4);
+            graph.AddEdge(0, 2, 3);
+
+            graph.AddEdge(1, 2, 5);
+            graph.AddEdge(1, 3, 2);
+
+            graph.AddEdge(2, 4, 8);
+            graph.AddEdge(2, 5, 1);
+
+            graph.AddEdge(3, 6, 8);
+
+            graph.AddEdge(4, 5, 6);
+            graph.AddEdge(4, 6, 2);
+
+            List<Edge> MinimumSpanningTree = graph.JarnikMST();
+
+            Console.WriteLine("Minimum spanning tree:");
+            foreach (Edge edge in MinimumSpanningTree)
+            {
+                Console.WriteLine($"{edge.From} - {edge.To} ({edge.Weight})");
+            }
         }
     }
+
+    
 }
